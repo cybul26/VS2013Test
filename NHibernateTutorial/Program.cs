73f8186d807f4470bc345644fdeef4d2691ed0ea
cybul26 <cybul26@gmail.com>
2014-10-18 13:27:48 +0200

    Test1

---
+++ NHibernateTutorial/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NHibernateTutorial.Domain;
+using NHibernate.Cfg;
+using NHibernate.Tool.hbm2ddl;
+
+namespace NHibernateTutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LoadNHibernateCfg();
+
+            /* CRUD */
+            CharacterRepository repo = new CharacterRepository();
+
+            //CREATE!
+            var MikeAbyss = new Character { Name = "MikeAbyss", HealthPoints = 700, Mana = 10, Profession = "Knight" };
+            repo.Add(MikeAbyss);
+
+            //READ!
+            Character mike = repo.GetCharacterByName("MikeAbyss");
+
+            //UPDATE!
+            mike.Name = "Cybul bóg";
+            repo.Update(mike);
+
+            //DELETE!
+            //repo.Delete(mike);
+
+            Console.ReadKey();
+        }
+
+        public static void LoadNHibernateCfg()
+        {
+            var cfg = new Configuration();
+            cfg.Configure();
+            cfg.AddAssembly(typeof(Character).Assembly);
+            new SchemaExport(cfg).Execute(true, true, false);
+        }
+
+    }
+}
